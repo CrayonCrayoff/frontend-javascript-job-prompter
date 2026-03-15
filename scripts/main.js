e6f8738bf0ffa36a43b20be7e6b1e7d172f898c6
CrayonCrayoff <krejt90@gmail.com>
2026-03-15 22:35:51 +0100

    Uitwerkingen opdracht 3

--- scripts/main.js
+++ scripts/main.js
@@ -12,21 +12,49 @@
 // // Opdracht 1d
 // console.log(`Sales is een uitdagende afdeling om te werken als Verkoopmanager. ${departments.sales.jobs[1].description}`);
 
-// Opdracht 2a
-const userInput = prompt("Over welke afdeling wil je meer informatie? Kies uit: [marketing / sales / customer-service]");
-console.log(userInput);
+// // Opdracht 2a
+// const userInput = prompt("Over welke afdeling wil je meer informatie? Kies uit: [marketing / sales / customer-service]");
+// console.log(userInput);
+//
+// // Opdracht 2b
+// let response = `Je koos ${userInput}. `;
+// switch (userInput) {
+//     case "marketing":
+//     case "sales":
+//     case "customer-service":
+//         response += `${departments[userInput].description}`;
+//         console.log(response);
+//         break;
+//     // Opdracht 2c
+//     default:
+//         response = "Ongeldige keuze. Probeer het opnieuw door de pagina te verversen";
+//         console.error(response);
+// }
 
-// Opdracht 2b
-let response = `Je koos ${userInput}. `;
-switch (userInput) {
-    case "marketing":
-    case "sales":
-    case "customer-service":
-        response += `${departments[userInput].description}`;
-        console.log(response);
+// Opdracht 3a
+let jobChoicePrompt = `Je koos marketing. Over welke functie wil je meer weten? Voer een getal tussen 0 en 3 in.`
+
+for (let i = 0; i < 4; i++) {
+    jobChoicePrompt += ` ${i}: [${departments.marketing.jobs[i].title}]`;
+    if (i < 3) {
+        jobChoicePrompt += ", "
+    }
+}
+
+const jobChoice = Number(prompt(jobChoicePrompt));
+
+// Opdracht 3b
+// Opdracht 3c
+
+switch (jobChoice) {
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+        console.log(
+            `Je koos ${departments.marketing.jobs[jobChoice].title}. Een uitdagende rol! ${departments.marketing.jobs[jobChoice].description}`
+        )
         break;
-    // Opdracht 2c
     default:
-        response = "Ongeldige keuze. Probeer het opnieuw door de pagina te verversen";
-        console.error(response);
+        console.error("Ongeldige input. Probeer het opnieuw door de pagina te verversen.")
 }
